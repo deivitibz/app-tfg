be82568e418d7dd90a2b7de8ebab00345e46556c
david perez <deivitwebmaster@gmail.com>
2021-01-27 23:32:34 +0100

    formulario de login creado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegistroComponent } from './components/registro/registro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/login'},
+  { path: 'login', component: LoginComponent},
+  { path: 'registro', component: RegistroComponent},
+  { path: '**', redirectTo: '/login'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
